0664e0dd79f435b69bfdf5eab7f8411747ff48a2
cake12532638 <searcx12@naver.com>
2021-07-05 14:58:02 +0900

    issue#1

--- Pyexpert.d/hello.py
+++ Pyexpert.d/hello.py
@@ -1,4 +1,12 @@
 #! /usr/bin/env python
 
-print('Hello Bioinformatics')
+import sys 
 
+#print(sys.argv)
+#sys.exit()
+
+name = sys.argv[1] #sys.argv 리스트에서 받아서 인덱스에 넣는다
+name2 = sys.argv[2]
+
+print(f"Hello {name}")
+print(f"Hello {name2}")
